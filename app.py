c0ac3391b1dc664a8221ab24f6db493af538da84
akasha04085-maker <akasha04085@gmail.com>
2026-06-11 17:44:21 +0530

    Completed file upload manager with download delete and UI improvements

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import os
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template,request
+from flask import send_from_directory
+from flask import redirect, url_for
+
 
 app = Flask(__name__)
 
@@ -17,7 +22,8 @@ def allowed_file(filename):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    files = os.listdir('uploads')
+    return render_template('index.html',files=files)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -29,8 +35,33 @@ def upload():
     if not allowed_file(file.filename):
         return "only PDF, PNG, JPG, JPEG FILES are allowed"
     
+    filename = secure_filename(file.filename)
     file.save(f'uploads/{file.filename}')
     
     return "File Uploaded Successfully"
+
+@app.route('/files')
+def files():
+    
+    uploaded_files = os.listdir('uploads')
+    
+    return{
+        "files": uploaded_files
+    }
+    
+@app.route('/download/<path:filename>')
+def download_file(filename):
+    return send_from_directory(
+        'uploads',
+        filename,
+        as_attachment=True
+    )
+    
+@app.route('/delete/<path:filename>')
+def delete_file(filename):
+    filepath = os.path.join('uploads', filename)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(debug=True)
