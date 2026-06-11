a48cf797e3132915c79ed4432ed31f70f5618387
akasha04085-maker <akasha04085@gmail.com>
2026-06-11 18:19:20 +0530

    add flask pop msg

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from werkzeug.utils import secure_filename
-from flask import Flask, render_template,request
-from flask import send_from_directory
-from flask import redirect, url_for
 
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+    flash
+)
+
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 
 ALLOWED_EXTENSIONS = {
     'pdf',
@@ -14,54 +22,80 @@ ALLOWED_EXTENSIONS = {
     'jpeg'
 }
 
+
 def allowed_file(filename):
-    return(
-        '.' in filename and 
+    return (
+        '.' in filename and
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
     )
 
+
 @app.route('/')
 def home():
-    files = os.listdir('uploads')
-    return render_template('index.html',files=files)
+
+    uploaded_files = os.listdir('uploads')
+
+    return render_template(
+        'index.html',
+        files=uploaded_files
+    )
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['file']
-    
-    if file.filename == '':
-        return "No File Selected"
-    
-    if not allowed_file(file.filename):
-        return "only PDF, PNG, JPG, JPEG FILES are allowed"
-    
-    filename = secure_filename(file.filename)
-    file.save(f'uploads/{file.filename}')
-    
-    return "File Uploaded Successfully"
 
-@app.route('/files')
-def files():
-    
-    uploaded_files = os.listdir('uploads')
-    
-    return{
-        "files": uploaded_files
-    }
-    
+    if 'file' not in request.files:
+        flash("❌ No file selected")
+        return redirect(url_for('home'))
+
+    file = request.files['file']
+
+    if file.filename == '':
+        flash("❌ No file selected")
+        return redirect(url_for('home'))
+
+    if not allowed_file(file.filename):
+        flash("❌ Only PDF, PNG, JPG, JPEG files are allowed")
+        return redirect(url_for('home'))
+
+    filename = secure_filename(file.filename)
+
+    file.save(
+        os.path.join(
+            'uploads',
+            filename
+        )
+    )
+
+    flash("✅ File Uploaded Successfully")
+
+    return redirect(url_for('home'))
+
+
 @app.route('/download/<path:filename>')
 def download_file(filename):
+
     return send_from_directory(
         'uploads',
         filename,
         as_attachment=True
     )
-    
+
+
 @app.route('/delete/<path:filename>')
 def delete_file(filename):
-    filepath = os.path.join('uploads', filename)
+
+    filepath = os.path.join(
+        'uploads',
+        filename
+    )
+
     if os.path.exists(filepath):
         os.remove(filepath)
+        flash("🗑 File Deleted Successfully")
+
     return redirect(url_for('home'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
